05364cf15c76508f148e4a9887f099f55a8c3a3a
keil <38099918+zzzkeil@users.noreply.github.com>
2020-03-08 10:39:27 +0100

    Update unfinished_add_website.sh

--- unfinished_add_website.sh
+++ unfinished_add_website.sh
@@ -18,12 +18,11 @@ if [[ "$EUID" -ne 0 ]]; then
 fi
 
 read -p "sitename: " -e -i example.domain servername
-randomkey1=$(date +%s)
+randomkey1=$(date +%s | cut -c 3-)
 read -p "sql databasename: " -e -i db$randomkey1 databasename
-randomkey2=$(date +%s)
-read -p "sql databaseuser: " -e -i dbuser$randomkey2 databaseuser
-randomkey3=$(</dev/urandom tr -dc 'A-Za-z0-9.:_' | head -c 12  ; echo)
-read -p "sql databaseuserpasswd: " -e -i $randomkey3 databaseuserpasswd
+read -p "sql databaseuser: " -e -i dbuser$randomkey1 databaseuser
+randomkey2=$(</dev/urandom tr -dc 'A-Za-z0-9.:_' | head -c 12  ; echo)
+read -p "sql databaseuserpasswd: " -e -i $randomkey2 databaseuserpasswd
 echo "
 $servername
 databasename : $databasename
