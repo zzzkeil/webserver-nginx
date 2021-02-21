939a87561b9e78e603fc210219abf08e7409c855
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-02-21 11:50:48 +0100

    Update add_website.sh

--- add_website.sh
+++ add_website.sh
@@ -17,14 +17,14 @@ if [[ "$EUID" -ne 0 ]]; then
 	exit 1
 fi
 
-read -p "sitename: " -e -i example.domain servername
+read -p "sitename: " -e -i example.domain sitename
 randomkey1=$(date +%s | cut -c 3-)
 read -p "sql databasename: " -e -i db$randomkey1 databasename
 read -p "sql databaseuser: " -e -i dbuser$randomkey1 databaseuser
 randomkey2=$(</dev/urandom tr -dc 'A-Za-z0-9.:_' | head -c 12  ; echo)
 read -p "sql databaseuserpasswd: " -e -i $randomkey2 databaseuserpasswd
 echo "
-$servername
+$sitename
 databasename : $databasename
 databaseuser : $databaseuser
 databaseuserpasswd : $databaseuserpasswd
@@ -33,7 +33,7 @@ databaseuserpasswd : $databaseuserpasswd
 
 ###
 function copy4SSL() {
-cp /etc/nginx/conf.d/$servername.conf /etc/nginx/conf.d/$servername.conf.orig
+cp /etc/nginx/conf.d/$sitename.conf /etc/nginx/conf.d/$sitename.conf.orig
 cp /etc/nginx/ssl.conf /etc/nginx/ssl.conf.orig
 }
 ###
@@ -50,14 +50,14 @@ echo ""
 }
 
 ###create folders
-mkdir -p /var/www/$servername/
+mkdir -p /home/$sitename/html
 ###create temp html and php file
-echo "<html><body><center>test HTML file</center></body></html>" >> /var/www/$servername/index.html
+echo "<html><body><center>test HTML file</center></body></html>" >> /home/$sitename/html/index.html
 echo "<?php
 phpinfo();
-?>" >> /var/www/$servername/info.php
+?>" >> /home/$sitename/html/info.php
 ###apply permissions
-chown -R www-data:www-data /var/www/$servername
+chown -R www-data:www-data /home/$sitename/html
 ### add database
 mysql -uroot <<EOF
 CREATE DATABASE $databasename CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
@@ -67,7 +67,7 @@ FLUSH privileges;
 EOF
 ###prepare NGINX for Site and SSL
 echo "server {
-server_name $servername;
+server_name $sitename;
 listen 80;
 listen [::]:80;
 location ^~ /.well-known/acme-challenge {
@@ -75,7 +75,7 @@ proxy_pass http://127.0.0.1:81;
 proxy_set_header Host \$host;
 }
 }
-" > /etc/nginx/conf.d/$servername.conf
+" > /etc/nginx/conf.d/$sitename.conf
 
 
 ###restart nginx php db
@@ -85,18 +85,18 @@ proxy_set_header Host \$host;
 
 
 ### letsencrypt 
-certbot certonly -a webroot --webroot-path=/var/www/letsencrypt --rsa-key-size 4096 -d $servername -d www.$servername 
-#certbot certonly --dry-run -a webroot --webroot-path=/var/www/letsencrypt --rsa-key-size 4096 -d $servername
+certbot certonly -a webroot --webroot-path=/var/www/letsencrypt --rsa-key-size 4096 -d $sitename -d www.$sitename 
+#certbot certonly --dry-run -a webroot --webroot-path=/var/www/letsencrypt --rsa-key-size 4096 -d $sitename
 
 if [ ! -d "/etc/letsencrypt/live" ]; then
 errorSSL
 else
 copy4SSL
-mv /etc/nginx/conf.d/$servername.conf /etc/nginx/conf.d/$servername.conf.bak
-touch /etc/nginx/conf.d/$servername.conf
-cat <<EOF >/etc/nginx/conf.d/$servername.conf
+mv /etc/nginx/conf.d/$sitename.conf /etc/nginx/conf.d/$sitename.conf.bak
+touch /etc/nginx/conf.d/$sitename.conf
+cat <<EOF >/etc/nginx/conf.d/$sitename.conf
 server {
-server_name $servername www.$servername;
+server_name $sitename www.$sitename;
 listen 80;
 listen [::]:80;
 location ^~ /.well-known/acme-challenge {
@@ -108,10 +108,10 @@ return 301 https://\$host\$request_uri;
 }
 }
 server {
-server_name $servername www.$servername;
+server_name $sitename www.$sitename;
 listen 443 ssl http2;
 listen [::]:443 ssl http2;
-root /var/www/$servername;
+root /home/$sitename/html;
 index index.php index.html index.htm;
 location / {
 		try_files \$uri \$uri/ =404;
@@ -120,25 +120,61 @@ location ~ \.php$ {
     include /etc/nginx/fastcgi_params;
     fastcgi_pass unix:/run/php/php8.0-fpm.sock;
     fastcgi_index index.php;
-    fastcgi_param SCRIPT_FILENAME var/www/$servername/\$fastcgi_script_name;
+    fastcgi_param SCRIPT_FILENAME /home/$sitename/html\$fastcgi_script_name;
 }
-# letsencrypt for $servername
-ssl_certificate /etc/letsencrypt/live/$servername/fullchain.pem;
-ssl_certificate_key /etc/letsencrypt/live/$servername/privkey.pem;
-ssl_trusted_certificate /etc/letsencrypt/live/$servername/chain.pem;
+# letsencrypt for $sitename
+ssl_certificate /etc/letsencrypt/live/$sitename/fullchain.pem;
+ssl_certificate_key /etc/letsencrypt/live/$sitename/privkey.pem;
+ssl_trusted_certificate /etc/letsencrypt/live/$sitename/chain.pem;
 #
 # logs
-access_log /var/log/nginx/$servername.access.log;
-error_log /var/log/nginx/$servername.error.log warn;
+access_log /var/log/nginx/$sitename.access.log;
+error_log /var/log/nginx/$sitename.error.log warn;
 #
 }
 EOF
 
 
-sed -i "s/server_name.*;/server_name $servername;/" /etc/nginx/conf.d/$servername.conf
+sed -i "s/server_name.*;/server_name $sitename;/" /etc/nginx/conf.d/$sitename.conf
 sed -i s/\#\ssl/\ssl/g /etc/nginx/ssl.conf
 fi
 systemctl restart nginx.service
+
+
+
+##############Testing####################################
+###add sftp user for this website
+echo "add sftp user for this website"
+echo
+echo
+randomkey3=$(date +%s | cut -c 4-)
+read -p "sftp username: " -e -i sftp$randomkey3 sftpuser
+randomkey4=$(</dev/urandom tr -dc 'A-Za-z0-9.:_' | head -c 32  ; echo)
+read -p "sql databaseuserpasswd: " -e -i $randomkey4 sftppasswd
+echo "
+$sitename
+sftpuser : $sftpuser
+sftppasswd : $sftppasswd
+#
+" >> /root/sftp_list.txt
+
+useradd -M -s /usr/sbin/nologin -d /home/$sitename/html -g www-data -p $(echo $sftppasswd | openssl passwd -1 -stdin) $sftpuser
+chown -R $sftpuser:www-data /home/$sitename/html
+chmod 0755 /home/$sitename/html
+
+
+echo "
+Match User $sftpuser
+ForceCommand internal-sftp
+PasswordAuthentication yes
+ChrootDirectory /home/$sitename/
+PermitTunnel no
+AllowAgentForwarding no
+AllowTcpForwarding no
+X11Forwarding no
+"
+>> /etc/ssh/sshd_config
+systemctl restart sshd
 
 ### CleanUp
 cat /dev/null > ~/.bash_history && history -c && history -w
