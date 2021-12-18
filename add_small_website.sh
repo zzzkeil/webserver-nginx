9e5202b5a1a25e167effa23832391f77228fb989
Klaus <38099918+zzzkeil@users.noreply.github.com>
2021-12-18 11:04:51 +0100

    Update add_small_website.sh

--- add_small_website.sh
+++ add_small_website.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-echo "3"
+echo "4"
 echo ""
 echo "To EXIT this script press  [ENTER]"
 echo 
@@ -51,21 +51,83 @@ chmod 775 /home/$sitename/html
 
 echo "
 <!doctype html>
-
 <html>
 <head>
   <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="index.css">
   <title>$sitename</title>
 </head>
-
 <body>
-
-<h1>Wellcome at $sitename</h1>
-<p>This is a placeholder</p> 
-
+<div class="bg"></div>
+<div class="bg bg2"></div>
+<div class="bg bg3"></div>
+<div class="content">
+<h1>Wellcome to $sitename</h1>
+<p>This is a placeholder<p>
+<p>I'll be back, soon .....<p>
+</div>
 </body>
 </html>
 " > /home/$sitename/html/index.html
+
+echo "
+html {
+  height:100%;
+}
+
+body {
+  margin:0;
+}
+
+.bg {
+  animation:slide 10s ease-in-out infinite alternate;
+  background-image: linear-gradient(-60deg, #6c3 50%, #09f 50%);
+  bottom:0;
+  left:-50%;
+  opacity:.5;
+  position:fixed;
+  right:-50%;
+  top:0;
+  z-index:-1;
+}
+
+.bg2 {
+  animation-direction:alternate-reverse;
+  animation-duration:20s;
+}
+
+.bg3 {
+  animation-duration:35s;
+}
+
+.content {
+  background-color:rgba(255,255,255,.8);
+  border-radius:.25em;
+  box-shadow:0 0 .25em rgba(0,0,0,.25);
+  box-sizing:border-box;
+  left:50%;
+  padding:10vmin;
+  position:fixed;
+  text-align:center;
+  top:50%;
+  transform:translate(-50%, -50%);
+}
+
+h1 {
+  font-family:monospace;
+}
+
+@keyframes slide {
+  0% {
+    transform:translateX(-25%);
+  }
+  100% {
+    transform:translateX(25%);
+  }
+}
+" > /home/$sitename/html/index.css 
+
 
 chown $siteuser:www-data /home/$sitename/html
 
