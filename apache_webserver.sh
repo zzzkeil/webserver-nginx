8abed4a4dc8f9c819424ca55d2eb2822e97f769d
keil <38099918+zzzkeil@users.noreply.github.com>
2025-03-25 19:41:54 +0000

    Create apache_webserver.sh

---
+++ apache_webserver.sh
@@ -0,0 +1,397 @@
+#!/bin/bash
+
+# visual text settings
+RED="\e[31m"
+GREEN="\e[32m"
+GRAY="\e[37m"
+YELLOW="\e[93m"
+
+REDB="\e[41m"
+GREENB="\e[42m"
+GRAYB="\e[47m"
+ENDCOLOR="\e[0m"
+
+
+clear
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}Advanced script to install apache webserver on Debian 12                   ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}Settings : TLSv1.3 only | lets encrypt ecdsa | other mods | php | mariadb  ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}My base_setup.sh script is needed to setup this script correctly!!         ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}If not installed, a automatic download starts, then follow the instructions${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}More information: https://github.com/zzzkeil/webserver                     ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR}                 Version 2025.03.23 - changelog on github                   ${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+echo ""
+echo ""
+echo ""
+echo  -e "                    ${RED}To EXIT this script press any key${ENDCOLOR}"
+echo ""
+echo  -e "                  ${GREEN}Press [Y] to begin  -  script not testet - ${ENDCOLOR}"
+read -p "" -n 1 -r
+echo ""
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    exit 1
+fi
+#
+### root check
+if [[ "$EUID" -ne 0 ]]; then
+	echo -e "${RED}Sorry, you need to run this as root${ENDCOLOR}"
+	exit 1
+fi
+
+### base_setup check
+if [[ -e /root/base_setup.README ]]; then
+     echo -e "base_setup script installed = ${GREEN}ok${ENDCOLOR}"
+	 else
+	 echo -e " ${YELLOW}Warning:${ENDCOLOR}"
+	 echo -e " ${YELLOW}You need to install my base_setup script first!${ENDCOLOR}"
+	 echo -e " ${YELLOW}Starting download base_setup.sh from my repository${ENDCOLOR}"
+	 echo ""
+	 echo ""
+	 wget -O  base_setup.sh https://raw.githubusercontent.com/zzzkeil/base_setups/master/base_setup.sh
+         chmod +x base_setup.sh
+	 echo ""
+	 echo ""
+         echo -e " Now run ${YELLOW}./base_setup.sh${ENDCOLOR} manualy and reboot, then run this script again."
+	 echo ""
+	 echo ""
+	 exit 1
+fi
+
+
+### check if Debian
+. /etc/os-release
+if [[ "$ID" = 'debian' ]]; then
+   echo -e "OS ID check = ${GREEN}ok${ENDCOLOR}"
+   else 
+   echo -e "${RED}This script is only for Debian ${ENDCOLOR}"
+   exit 1
+fi
+
+###global function to update and cleanup the environment
+function update_and_clean() {
+apt update
+apt upgrade -y
+apt autoclean -y
+apt autoremove -y
+}
+
+
+### START ###
+# Debian 12
+if [[ "$VERSION_ID" = '12' ]]; then
+apt install curl gnupg2 ca-certificates apt-transport-https lsb-release debian-archive-keyring zlib1g imagemagick libxml2 memcached unzip libmagickcore-6.q16-6-extra -y
+
+###sury apache repo 
+curl -sSLo /tmp/debsuryorg-archive-keyring.deb https://packages.sury.org/debsuryorg-archive-keyring.deb
+dpkg -i /tmp/debsuryorg-archive-keyring.deb
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/apache2/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/apache2.list'
+###sury php repo
+sh -c 'echo "deb [signed-by=/usr/share/keyrings/debsuryorg-archive-keyring.gpg] https://packages.sury.org/php/ $(lsb_release -sc) main" > /etc/apt/sources.list.d/php.list'
+
+###maria-db repo
+mkdir -p /etc/apt/keyrings
+curl -o /etc/apt/keyrings/mariadb-keyring.pgp 'https://mariadb.org/mariadb_release_signing_key.pgp'
+echo "
+# MariaDB 11.4 repository list - created 2025-03-23 09:39 UTC
+# https://mariadb.org/download/
+X-Repolib-Name: MariaDB
+Types: deb
+# deb.mariadb.org is a dynamic mirror if your preferred mirror goes offline. See https://mariadb.org/mirrorbits/ for details.
+# URIs: https://deb.mariadb.org/11.4/debian
+URIs: https://mirror1.hs-esslingen.de/pub/Mirrors/mariadb/repo/11.4/debian
+Suites: bookworm
+Components: main
+Signed-By: /etc/apt/keyrings/mariadb-keyring.pgp
+" > /etc/apt/sources.list.d/mariadb.sources
+
+
+update_and_clean
+###php overflow 4 WP and NC......
+apt install apache2 certbot python3-certbot-apache mariadb-server php8.3 php8.3-fpm php8.3-cli php-mbstring php8.3-curl php8.3-igbinary php8.3-imagick php8.3-ssh2 php8.3-intl php8.3-mbstring php8.3-xml php8.3-zip php8.3-apcu php8.3-memcached php8.3-opcache php8.3-redis php8.3-mysql php8.3-gd php8.3-gmp php8.3-bcmath php8.3-bz2 php8.3-common -y
+fi
+
+###enable NGINX autostart
+systemctl enable apache2.service
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo "not ready"
+exit 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+### prepare the NGINX
+mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak && touch /etc/nginx/nginx.conf
+echo "user www-data;
+worker_processes auto;
+pid /var/run/nginx.pid;
+events {
+worker_connections 1024;
+multi_accept on;
+use epoll;
+}
+http {
+server_names_hash_bucket_size 64;
+set_real_ip_from 127.0.0.1;
+real_ip_header X-Forwarded-For;
+real_ip_recursive on;
+include /etc/nginx/mime.types;
+#include /etc/nginx/proxy.conf;
+#include /etc/nginx/ssl.conf;
+#include /etc/nginx/header.conf;
+#include /etc/nginx/optimization.conf;
+default_type application/octet-stream;
+access_log /var/log/nginx/access.log;
+error_log /var/log/nginx/error.log warn;
+sendfile on;
+send_timeout 3600;
+tcp_nopush on;
+tcp_nodelay on;
+open_file_cache max=500 inactive=10m;
+open_file_cache_errors on;
+keepalive_timeout 65;
+reset_timedout_connection on;
+server_tokens off;
+resolver 127.0.0.53 valid=30s;
+resolver_timeout 5s;
+client_max_body_size 2G;
+include /etc/nginx/conf.d/*.conf;
+}
+" > /etc/nginx/nginx.conf
+
+###create folders
+##mkdir -p /var/www/letsencrypt /etc/letsencrypt/rsa-certs /etc/letsencrypt/ecc-certs
+
+###apply permissions
+##chown -R www-data:www-data /var/www
+
+
+###restart apache
+systemctl restart apache2.service
+
+###install self signed certificates
+apt install ssl-cert -y
+
+###prepare NGINX for Site and SSL
+[ -f /etc/nginx/conf.d/default.conf ] && mv /etc/nginx/conf.d/default.conf /etc/nginx/conf.d/default.conf.bak
+touch /etc/nginx/conf.d/default.conf
+
+###create a Let's Encrypt vhost file
+touch /etc/nginx/conf.d/letsencrypt.conf
+echo "server
+{
+server_name 127.0.0.1;
+listen 127.0.0.1:81 default_server;
+charset utf-8;
+location ^~ /.well-known/acme-challenge
+{
+default_type text/plain;
+root /var/www/letsencrypt;
+}
+}
+" > /etc/nginx/conf.d/letsencrypt.conf
+
+
+###create a ssl configuration file
+echo "ssl_dhparam /etc/ssl/certs/dhparam.pem;
+ssl_session_timeout 60m;
+ssl_session_cache shared:SSL:30m;
+ssl_session_tickets off;
+# Mozilla modern configuration 2020 - the client chose the ciphers in TLSv1.3 only mode,  ok ....
+ssl_protocols TLSv1.3;
+ssl_prefer_server_ciphers off;
+#ssl_ecdh_curve X448:secp521r1:secp384r1;
+ssl_stapling on;
+ssl_stapling_verify on;
+#
+ssl_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
+ssl_certificate_key /etc/ssl/private/ssl-cert-snakeoil.key;
+ssl_trusted_certificate /etc/ssl/certs/ssl-cert-snakeoil.pem;
+" > /etc/nginx/ssl.conf
+
+###add a default dhparam.pem file // https://wiki.mozilla.org/Security/Server_Side_TLS#ffdhe4096
+
+clear
+echo ""
+echo -e " ${YELLOW}Get some coffee, restore your energy, this can take a while or just seconds :) ${ENDCOLOR}"
+echo ""
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 4096
+clear
+echo -e " ${GREEN}:) done ${ENDCOLOR}"
+
+###create a proxy configuration file
+echo "proxy_set_header Host \$host;
+proxy_set_header X-Real-IP \$remote_addr;
+proxy_set_header X-Forwarded-Host \$host;
+proxy_set_header X-Forwarded-Protocol \$scheme;
+proxy_set_header X-Forwarded-For \$remote_addr;
+proxy_set_header X-Forwarded-Port \$server_port;
+proxy_set_header X-Forwarded-Server \$host;
+proxy_connect_timeout 3600;
+proxy_send_timeout 3600;
+proxy_read_timeout 3600;
+proxy_redirect off;
+"  > /etc/nginx/proxy.conf
+
+
+###create a header configuration file
+echo 'add_header Strict-Transport-Security "max-age=15768000; includeSubDomains; preload;";
+add_header X-Robots-Tag none; add_header X-Download-Options noopen;
+add_header X-Permitted-Cross-Domain-Policies none;
+add_header X-Content-Type-Options "nosniff" always;
+add_header X-XSS-Protection "1; mode=block" always;
+add_header Referrer-Policy "no-referrer" always;
+add_header X-Frame-Options "SAMEORIGIN";
+'  > /etc/nginx/header.conf
+
+
+###create a nginx optimization file
+echo 'fastcgi_hide_header X-Powered-By;
+fastcgi_read_timeout 3600;
+fastcgi_send_timeout 3600;
+fastcgi_connect_timeout 3600;
+fastcgi_buffers 64 64K;
+fastcgi_buffer_size 256k;
+fastcgi_busy_buffers_size 3840K;
+fastcgi_cache_key \$http_cookie\$request_method\$host\$request_uri;
+fastcgi_cache_use_stale error timeout invalid_header http_500;
+fastcgi_ignore_headers Cache-Control Expires Set-Cookie;
+gzip on;
+gzip_vary on;
+gzip_comp_level 4;
+gzip_min_length 256;
+gzip_proxied expired no-cache no-store private no_last_modified no_etag auth;
+gzip_types application/atom+xml application/javascript application/json application/ld+json application/manifest+json application/rss+xml application/vnd.geo+json application/vnd.ms-fontobject application/x-font-ttf application/x-web-app-manifest+json application/xhtml+xml application/xml font/opentype image/bmp image/svg+xml image/x-icon text/cache-manifest text/css text/plain text/vcard text/vnd.rim.location.xloc text/vtt text/x-component text/x-cross-domain-policy;
+gzip_disable "MSIE [1-6]\.";
+' > /etc/nginx/optimization.conf
+
+
+
+###enable all nginx configuration files
+sed -i s/\#\include/\include/g /etc/nginx/nginx.conf
+
+###restart NGINX
+/usr/sbin/service nginx restart
+
+###php mods
+sed -i "s/memory_limit = 128M/memory_limit = 512M/" /etc/php/8.3/fpm/php.ini
+sed -i "s/output_buffering =.*/output_buffering = '0'/" /etc/php/8.3/fpm/php.ini
+sed -i "s/max_execution_time =.*/max_execution_time = 360/" /etc/php/8.3/fpm/php.ini
+sed -i "s/max_input_time =.*/max_input_time = 360/" /etc/php/8.3/fpm/php.ini
+sed -i "s/post_max_size =.*/post_max_size = 2G/" /etc/php/8.3/fpm/php.ini
+sed -i "s/upload_max_filesize =.*/upload_max_filesize = 2G/" /etc/php/8.3/fpm/php.ini
+#sed -i "s/;date.timezone.*/date.timezone = Europe\/\Berlin/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;cgi.fix_pathinfo.*/cgi.fix_pathinfo=0/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;session.cookie_secure.*/session.cookie_secure = True/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.enable=.*/opcache.enable=1/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.validate_timestamps=.*/opcache.validate_timestamps=0/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.enable_cli=.*/opcache.enable_cli=1/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.memory_consumption=.*/opcache.memory_consumption=256/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.interned_strings_buffer=.*/opcache.interned_strings_buffer=64/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.max_accelerated_files=.*/opcache.max_accelerated_files=100000/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.revalidate_freq=.*/opcache.revalidate_freq=60/" /etc/php/8.3/fpm/php.ini
+sed -i "s/;opcache.save_comments=.*/opcache.save_comments=1/" /etc/php/8.3/fpm/php.ini
+sed -i "s/max_file_uploads =.*/max_file_uploads = 20/" /etc/php/8.3/fpm/php.ini
+sed -i '$aopcache.jit=1255' /etc/php/8.3/fpm/php.ini
+sed -i '$aopcache.jit_buffer_size=256M' /etc/php/8.3/fpm/php.ini
+sed -i '$aapc.enable_cli=1' /etc/php/8.3/fpm/php.ini
+sed -i '$aapc.enable_cli=1' /etc/php/8.3/mods-available/apcu.ini
+sed -i '$aopcache.jit=1255' /etc/php/8.3/mods-available/opcache.ini
+sed -i '$aopcache.jit_buffer_size=256M' /etc/php/8.3/mods-available/opcache.ini
+sed -i '$a[mysql]' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.allow_local_infile=On' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.allow_persistent=On' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.cache_size=2000' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.max_persistent=-1' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.max_links=-1' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.connect_timeout=60' /etc/php/8.3/mods-available/mysqli.ini
+sed -i '$amysql.trace_mode=Off' /etc/php/8.3/mods-available/mysqli.ini
+
+
+
+
+###mariadb install
+
+echo "--------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------------------------"
+read -p "Set your mariaDB port: " -e -i 3306 dbport
+echo "--------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------------------------"
+mv /etc/mysql/my.cnf /etc/mysql/my.cnf.bak
+echo "
+[mysqld]
+bind-address = 127.0.0.1
+port = $dbport
+
+slow_query_log_file    = /var/log/mysql/mariadb-slow.log
+long_query_time        = 10
+log_slow_rate_limit    = 1000
+log_slow_verbosity     = query_plan
+log-queries-not-using-indexes
+" > /etc/mysql/my.cnf
+
+
+echo "--------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------------------------"
+echo " Your database server will now be hardened - just follow the instructions."
+echo " Keep in mind: your MariaDB root password is still NOT set !"
+echo -e "${YELLOW} You should set a root password, when asked${ENDCOLOR}"
+echo "--------------------------------------------------------------------------------------------------------"
+echo "--------------------------------------------------------------------------------------------------------"
+mariadb-secure-installation
+
+systemctl restart mariadb.service
+systemctl restart php8.3-fpm
+
+
+
+cd
+wget -O  nginx_add_website.sh https://raw.githubusercontent.com/zzzkeil/webserver/master/nginx_add_website.sh
+chmod +x nginx_add_website.sh
+
+### open ports firewalld
+firewall-cmd --zone=public --add-port=80/tcp
+firewall-cmd --zone=public --add-port=443/tcp
+firewall-cmd --runtime-to-permanent
+
+clear
+
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}To add your first website run ./add_small_website.sh                       ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
+echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
+
+### CleanUp
+cat /dev/null > ~/.bash_history && history -c && history -w
+exit 0
