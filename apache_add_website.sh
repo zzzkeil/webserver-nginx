9187fdb5f000ad905038652ddf01920290a64ac8
keil <38099918+zzzkeil@users.noreply.github.com>
2025-03-30 13:35:56 +0000

    Update apache_add_website.sh

--- apache_add_website.sh
+++ apache_add_website.sh
@@ -41,14 +41,15 @@ fi
 
 ### site data
 randomkeyuser=$(</dev/urandom tr -dc 'A-Za-z0-9._' | head -c 32  ; echo)
-randomkey1=$(date +%s | cut -c 3-)
-randomkey2=$(</dev/urandom tr -dc 'A-Za-z0-9._' | head -c 32  ; echo)
+randomkey1=$(date +%s | cut -c 7-)
+randomkey2=$(shuf -i 1000-9999 -n 1)
+randomkey3=$(</dev/urandom tr -dc 'A-Za-z0-9._' | head -c 32  ; echo)
 read -p "sitename: " -e -i example.domain sitename
-read -p "siteuser: " -e -i user$randomkey1 siteuser
+read -p "siteuser: " -e -i user$randomkey1$randomkey2 siteuser
 read -p "userpass: " -e -i $randomkeyuser userpass
-read -p "sql databasename: " -e -i db$randomkey1 databasename
-read -p "sql databaseuser: " -e -i dbuser$randomkey1 databaseuser
-read -p "sql databaseuserpasswd: " -e -i $randomkey2 databaseuserpasswd
+read -p "sql databasename: " -e -i db$randomkey2$randomkey1 databasename
+read -p "sql databaseuser: " -e -i dbuser$randomkey1$randomkey2 databaseuser
+read -p "sql databaseuserpasswd: " -e -i $randomkey3 databaseuserpasswd
 
 
 ###create sftp user
@@ -72,20 +73,18 @@ chmod 775 /home/www/$sitename/html
 
 cat <<EOF >> /etc/apache2/sites-available/$sitename.conf
 <VirtualHost *:80>
-   ServerName $sitename
-    RewriteEngine On
-    DocumentRoot /home/www/$sitename/html    
- <Directory /home/www/$sitename/html>
+  ServerName $sitename
+  RewriteEngine On
+  DocumentRoot /home/www/$sitename/html    
+<Directory /home/www/$sitename/html>
   Options Indexes FollowSymLinks
   AllowOverride All
   Require all granted
 </Directory>
- ErrorLog /home/www/$sitename/error.log
- CustomLog /home/www/$sitename/access.log combined
+  ErrorLog /home/www/$sitename/error.log
+  CustomLog /home/www/$sitename/access.log combined
 </VirtualHost>
 EOF
-
-
 
 mariadb -uroot <<EOF
 CREATE DATABASE $databasename CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
@@ -94,94 +93,29 @@ GRANT ALL PRIVILEGES on $databasename.* to '$databaseuser'@'localhost' identifie
 FLUSH privileges;
 EOF
 
-
 echo "
 <!doctype html>
 <html>
 <head>
   <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="index.css">
   <title>$sitename</title>
 </head>
 <body>
-<div class="bg"></div>
-<div class="bg bg2"></div>
-<div class="bg bg3"></div>
-<div class="content">
+<center>
 <h1>Wellcome to $sitename</h1>
-<p>This is a placeholder<p>
+<p>maintenance mode<p>
 <p>I'll be back, soon .....<p>
-</div>
+</center>
 </body>
 </html>
 " > /home/www/$sitename/html/index.html
-
 
 echo "
 <?php
 phpinfo();
 ?>
 " > /home/www/$sitename/html/checkphp.php
-
-
-echo "
-html {
-  height:100%;
-}
-
-body {
-  margin:0;
-}
-
-.bg {
-  animation:slide 10s ease-in-out infinite alternate;
-  background-image: linear-gradient(-60deg, #6c3 50%, #09f 50%);
-  bottom:0;
-  left:-50%;
-  opacity:.5;
-  position:fixed;
-  right:-50%;
-  top:0;
-  z-index:-1;
-}
-
-.bg2 {
-  animation-direction:alternate-reverse;
-  animation-duration:20s;
-}
-
-.bg3 {
-  animation-duration:35s;
-}
-
-.content {
-  background-color:rgba(255,255,255,.8);
-  border-radius:.25em;
-  box-shadow:0 0 .25em rgba(0,0,0,.25);
-  box-sizing:border-box;
-  left:50%;
-  padding:10vmin;
-  position:fixed;
-  text-align:center;
-  top:50%;
-  transform:translate(-50%, -50%);
-}
-
-h1 {
-  font-family:monospace;
-}
-
-@keyframes slide {
-  0% {
-    transform:translateX(-25%);
-  }
-  100% {
-    transform:translateX(25%);
-  }
-}
-" > /home/www/$sitename/html/index.css 
-
+ 
 chown -R $siteuser:www-data /home/www/$sitename/html
 
 a2ensite $sitename.conf
@@ -192,7 +126,6 @@ certbot --apache --agree-tos --register-unsafely-without-email --key-type ecdsa 
 
 systemctl restart apache2.service
 systemctl restart sshd.service
-
 
 echo "
 $sitename
@@ -205,11 +138,9 @@ Databaseuserpasswd : $randomkey2
 
 " >> /root/website_user_list.txt
 
-
 echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
 echo -e " ${GRAYB}#${ENDCOLOR} ${GREEN}Done. Test your site now.                                                  ${ENDCOLOR}${GRAYB}#${ENDCOLOR}"
 echo -e " ${GRAYB}##############################################################################${ENDCOLOR}"
-
 
 ### CleanUp
 cat /dev/null > ~/.bash_history && history -c && history -w
